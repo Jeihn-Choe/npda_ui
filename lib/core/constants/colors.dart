78baee9f10915d01ed06123747e7c198f5cfa6bb
JeihnChoe <JeihnChoe@novatek.kr>
2025-08-20 16:46:28 +0900

    initial commit

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Main Colors
+  static const Color celltrionGreen = Color(0xFF3EAE2C); // Pantone 361C [1, 2]
+  static const Color celltrionBlack = Color(0xFF1D252D); // Pantone 433C [3, 4, 5]
+
+  // Sub Colors
+  static const Color orange = Color(0xFFFF6B00);       // Pantone 1505C [6]
+  static const Color darkGrey = Color(0xFF898B8E);       // Pantone Cool Grey 8C [7, 8]
+  static const Color purple = Color(0xFF773DBC);       // Pantone 266C [9]
+  static const Color gold = Color(0xFF85714D);         // Pantone 872C [10, 11]
+  static const Color lightGrey = Color(0xFF8A8D8F);      // Pantone 877C [12, 13]
+
+  // Standard Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color transparent = Colors.transparent;
+
+  // Grayscale
+  static const Color grey100 = Color(0xFFF5F5F5);
+  static const Color grey200 = Color(0xFFEEEEEE);
+  static const Color grey300 = Color(0xFFE0E0E0);
+  static const Color grey400 = Color(0xFFBDBDBD);
+  static const Color grey500 = Color(0xFF9E9E9E);
+  static const Color grey600 = Color(0xFF757575);
+  static const Color grey700 = Color(0xFF616161);
+  static const Color grey800 = Color(0xFF424242);
+  static const Color grey900 = Color(0xFF212121);
+
+  // Semantic Colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFFFA000);
+}
